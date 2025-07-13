1c2aa489e525f82e59b81807abdbf25a4d694caf
AlePalu <alessandropalummo@gmail.com>
2025-07-13 08:43:18 +0200

    updated scripts

--- scripts/DEPDE_2D.R
+++ scripts/DEPDE_2D.R
@@ -1,176 +1,117 @@
-## ----setup, include = FALSE---------------------------------------------------
-knitr::opts_chunk$set(
-  echo = TRUE,
-  fig.align = "center",
-  collapse = TRUE,
-  comment = "#>",
-  message = FALSE,
-  warning = FALSE,
-  root.dir = "$(pwd)/../"
-)
-
-
-## ----fdaPDE, eval=TRUE--------------------------------------------------------
 # Import the fdaPDE library
 library(fdaPDE2)           # v. 2.0 (2025)
 rm(list = ls())
 
 # Load additional libraries and helper functions for plotting
 source("../utils/graphics.R")
-
-
-## ----theme, echo=FALSE--------------------------------------------------------
-theme_set(theme_minimal() +
-  theme(
-    axis.title = element_text(),
-    axis.text = element_text(),
-    axis.ticks = element_line(),
-    panel.grid = element_line()
-  )
+theme_set(
+  theme_minimal() +
+    theme(
+      axis.title = element_text(),
+      axis.text = element_text(),
+      axis.ticks = element_line(),
+      panel.grid = element_line()
+    )
 )
 
+######### GEOMETRY
 
-## ----spatial_domain-----------------------------------------------------------
-## [SPATIAL DOMAIN]
-# Load the boundary nodes
+# Load physical domain
 boundary_nodes <- read.table(file = "../data/DEPDE_2D/DEPDE_2D_boundary_nodes.txt")
-head(boundary_nodes)
-
-# Load the boundary segments
 boundary_segments <- read.table(file = "../data/DEPDE_2D/DEPDE_2D_boundary_segments.txt")
-head(boundary_segments)
 
-
-## ----mapview_boundary, fig.width=8.3, fig.height=5----------------------------
-# Convert boundary into a sf object
-boundary_nodes_sf <- st_as_sf(x = rbind(boundary_nodes, boundary_nodes[1,]),
-                              coords = c("lon", "lat"), crs = 4326)
+# plot
+boundary_nodes_sf <- st_as_sf(
+  x = rbind(boundary_nodes, boundary_nodes[1,]),
+  coords = c("lon", "lat"), crs = 4326
+)
 boundary_polygon <- st_polygon(list(st_coordinates(boundary_nodes_sf)))
 boundary_sfc <- st_sfc(geometry = boundary_polygon, crs = 4326)
 boundary_sf <- st_sf(geometry = boundary_sfc)
 
 # Interactive plot
-mapview(boundary_sf,
-        col.regions = "gray25", alpha.regions = 0.25, col = "black", lwd = 1.5,
-        legend = FALSE, layer.name = "domain")
+mapview(
+  boundary_sf,
+  col.regions = "gray25", alpha.regions = 0.25, col = "black", lwd = 1.5,
+  legend = FALSE, layer.name = "domain"
+)
 
-
-## ----mesh---------------------------------------------------------------------
-## [MESH]
+# meshing (using RTriangle)
 # Create a planar straight line graph object
 boundary_pslg <- pslg(P = boundary_nodes, S = boundary_segments)
-
-# Create a regular mesh of the spatial domain
 mesh <- triangulate(p = boundary_pslg)
 if (is.null(mesh$H)) mesh$H <- matrix(data = numeric(0), ncol = 2)
-
-# Refine the mesh of the spatial domain
 mesh <- triangulate(p = mesh, a = 0.0001, q = 20, D = TRUE)
-if (is.null(mesh$H)) mesh$H <- matrix(data = numeric(0), ncol = 2)
 
-# Number of nodes
-nrow(mesh$P)
+# create a triangulation object
+mesh <- triangulation(
+  nodes = mesh$P, 
+  cells = mesh$T,
+  boundary = mesh$PB
+)
 
-# Number of triangles
-nrow(mesh$T)
+# plot
+mapview(
+  boundary_sf,
+  col.regions = "gray25", alpha.regions = 0.25, col = "black", lwd = 1.5,
+  legend = FALSE, layer = "domain") +
+mapview(
+  mesh, 
+  crs = 4326, col.regions = "transparent", lwd = 1.25, legend = FALSE, 
+  layer = "mesh"
+)
 
+######### DATA
 
-## ----triangulation------------------------------------------------------------
-# Set up the triangulation for fdaPDE
-mesh <- triangulation(nodes = mesh$P, cells = mesh$T,
-                      boundary = mesh$PB)
-
-# Nodes coordinates
-head(mesh$nodes)
-
-# Number of nodes
-mesh$n_nodes
-
-# Edges
-head(mesh$edges)
-
-# Number of edges
-mesh$n_edges
-
-# Triangles
-head(mesh$cells)
-
-# Number of triangles
-mesh$n_cells
-
-# Bounding box
-mesh$bbox
-
-
-## ----mapview_mesh, fig.width=8.3, fig.height=5--------------------------------
-# Interactive plot
-mapview(boundary_sf,
-        col.regions = "gray25", alpha.regions = 0.25, col = "black", lwd = 1.5,
-        legend = FALSE, layer = "domain") +
-  mapview(mesh, crs = 4326,
-          col.regions = "transparent", lwd = 1.25, legend = FALSE, layer = "mesh")
-
-
-## ----geoframe-----------------------------------------------------------------
-# Create the geoframe
+# create a geoframe object
 hampshire <- geoframe(domain = mesh)
 hampshire
-
-
-## ----data---------------------------------------------------------------------
-## [DATA]
-# Load the data
 data <- read.table(file = "../data/DEPDE_2D/DEPDE_2D_data.txt")
 head(data)
-
-# Focus on cases occurred in 2001 (first 2500 cases)
 data <- data[1:2500,]
 
-# Add layer with data to the geoframe object
-hampshire$insert(layer = "diseases", type = "point", geo = c("lon", "lat"), data = data)
+# add point pattern layer to the geoframe
+hampshire$insert(
+  layer = "diseases", type = "point", geo = c("lon", "lat"), data = data
+)
 hampshire
 
+# plot
+mapview(
+  hampshire[["diseases"]], varnames = "", 
+  crs = 4326, col.regions = "red3", cex = 2.5, layer.name = "locations") +
+mapview(
+  boundary_sf,
+  col.regions = "gray25", alpha.regions = 0.25, col = "black", lwd = 1.5,
+  legend = FALSE, layer = "domain"
+) 
 
-## ----mapview_data, fig.width=8.3, fig.height=5--------------------------------
-# Interactive plot
-mapview(hampshire[["diseases"]], varnames = "", crs = 4326, col.regions = "red3",
-        cex = 2.5, layer.name = "locations") +
- mapview(boundary_sf,
-         col.regions = "gray25", alpha.regions = 0.25, col = "black", lwd = 1.5,
-         legend = FALSE, layer = "domain") 
+######### MODELING
+# point pattern estimation model
 
-
-## ----density, results="hide"--------------------------------------------------
-## [DENSITY ESTIMATION]
-# Proposed value for the smoothing parameter
+# smoothing parameter proposal
 lambda_fixed <- 1e-2
 
-# Density estimation model
 model <- ppe(data = hampshire)
-
-# Density estimation fit
 fit <- model$fit(
   lambda = lambda_fixed,
   optimizer = bfgs()
 )
 
+head(model$density)     # Density estimates at mesh nodes
+head(model$log_density) # Log-density estimates at mesh nodes
 
-## ----density_output-----------------------------------------------------------
-# Density estimates at mesh nodes
-head(model$density)
-
-# Log-density estimates at mesh nodes
-head(model$log_density)
-
-# Finite element function
+# define a finite element function for the log_density field
 f <- fe_function(domain =  mesh, type = "P1", coeff = model$log_density)
 
-
-## ----mapview_SST_iso_grid, fig.width=8.3, fig.height=5------------------------
-# Interactive plot
-mapview(f, crs = 4326, col.regions = inferno,
-        na.color = "transparent", layer.name = "ESTIMATE") +
-  mapview(boundary_sf,
-        col.regions = "gray25", alpha.regions = 0.25, col = "black", lwd = 2,
-        legend = FALSE, layer.name = "domain")
+# plot
+mapview(
+  f, 
+  crs = 4326, col.regions = inferno, na.color = "transparent", 
+  layer.name = "ESTIMATE") +
+mapview(
+  boundary_sf,
+  col.regions = "gray25", alpha.regions = 0.25, col = "black", lwd = 2,
+  legend = FALSE, layer.name = "domain"
+)
 
--- scripts/QSRPDE_2D.R
+++ scripts/QSRPDE_2D.R
@@ -1,260 +1,227 @@
-## ----setup, include = FALSE---------------------------------------------------
-knitr::opts_chunk$set(
-  echo = TRUE,
-  fig.align = "center",
-  collapse = TRUE,
-  comment = "#>",
-  message = FALSE,
-  warning = FALSE,
-  root.dir = "$(pwd)/../"
-)
-
-
-## ----fdaPDE, eval=TRUE--------------------------------------------------------
 # Import the fdaPDE library
 library(fdaPDE2)           # v. 2.0 (2025)
 rm(list = ls())
 
 # Load additional libraries and helper functions for plotting
 source("../utils/graphics.R")
-
-
-## ----theme, echo=FALSE--------------------------------------------------------
-theme_set(theme_minimal() +
-  theme(
-    axis.title = element_text(),
-    axis.text = element_text(),
-    axis.ticks = element_line(),
-    panel.grid = element_line()
-  )
+theme_set(
+  theme_minimal() +
+    theme(
+      axis.title = element_text(),
+      axis.text = element_text(),
+      axis.ticks = element_line(),
+      panel.grid = element_line()
+    )
 )
 
+######### GEOMETRY
 
-## ----domain-------------------------------------------------------------------
-## [SPATIAL DOMAIN]
-# Load the boundary nodes
-boundary_nodes <- read.table(file = "../data/QSRPDE_2D/QSRPDE_2D_boundary_nodes.txt",
-                             header = TRUE)
-
+# Load physical domain
+boundary_nodes <- read.table(
+  file = "../data/QSRPDE_2D/QSRPDE_2D_boundary_nodes.txt",
+  header = TRUE
+)
 # Create boundary segments (consecutive boundary nodes are connected)
 boundary_segments <- cbind(1:(nrow(boundary_nodes)-1), 2:nrow(boundary_nodes))
-
-# Close the loop (connect the last node to the first)
 boundary_segments <- rbind(boundary_segments, c(nrow(boundary_nodes), 1))
 
-
-## ----mapview_boundary, fig.width=8.3, fig.height=5----------------------------
-# Convert boundary into a sf object
-boundary_nodes_sf <- st_as_sf(x = rbind(boundary_nodes, boundary_nodes[1,]),
-                              coords = c("lon", "lat"), crs = 4326)
+# plot
+boundary_nodes_sf <- st_as_sf(
+  x = rbind(boundary_nodes, boundary_nodes[1,]),
+  coords = c("lon", "lat"), crs = 4326
+)
 boundary_polygon <- st_polygon(list(st_coordinates(boundary_nodes_sf)))
 boundary_sfc <- st_sfc(geometry = boundary_polygon, crs = 4326)
 boundary_sf <- st_sf(geometry = boundary_sfc)
 
 # Interactive plot
-mapview(boundary_sf,
-        col.regions = "gray25", alpha.regions = 0.25, col = "black", lwd = 1.5,
-        legend = FALSE, layer.name = "domain")
+mapview(
+  boundary_sf,
+  col.regions = "gray25", alpha.regions = 0.25, col = "black", lwd = 1.5,
+  legend = FALSE, layer.name = "domain"
+)
 
-
-## ----geometry, fig.width=8.3, fig.height=5------------------------------------
-# Define a planar straight-line graph object
+# meshing (using RTriangle)
 p <- pslg(P = boundary_nodes, S = boundary_segments)
-
-# Create a regular mesh of the spatial domain
 mesh <- triangulate(p = p, Y = FALSE, D = TRUE)
 if (is.null(mesh$H)) mesh$H <- matrix(numeric(0), ncol = 2)
-
-# Refine the mesh of the spatial domain
+# mesh refinement
 mesh <- triangulate(mesh, a = 0.0045, q = 30, D = TRUE)
 
+mesh = triangulation(
+  nodes = mesh$P, 
+  cells = mesh$T, 
+  boundary = mesh$PB
+)
 
-## ----triangulation, fig.width=8.3, fig.height=5-------------------------------
-# Set up the triangulation for fdaPDE
-mesh = triangulation(nodes = mesh$P, cells = mesh$T, boundary = mesh$PB)
+# plot
+mapview(
+  boundary_sf,
+  col.regions = "gray25", alpha.regions = 0.25, col = "black", lwd = 1.5,
+  legend = FALSE, layer = "domain") +
+mapview(
+  mesh, 
+  crs = 4326, col.regions = "transparent", lwd = 1.25, legend = FALSE, 
+  layer = "mesh"
+)
 
-# Nodes coordinates
-head(mesh$nodes)
+######### DATA
 
-# Number of nodes
-mesh$n_nodes
-
-# Edges
-head(mesh$edges)
-
-# Number of edges
-mesh$n_edges
-
-# Triangles
-head(mesh$cells)
-
-# Number of triangles
-mesh$n_cells
-
-# Bounding box
-mesh$bbox
-
-
-## ----mesh, fig.width=8.3, fig.height=5----------------------------------------
-# Interactive plot
-mapview(boundary_sf,
-        col.regions = "gray25", alpha.regions = 0.25, col = "black", lwd = 1.5,
-        legend = FALSE, layer = "domain") +
-  mapview(mesh, crs = 4326,
-          col.regions = "transparent", lwd = 1.25, legend = FALSE, layer = "mesh")
-
-
-## ----data, fig.width=8.3, fig.height=5----------------------------------------
+# create a geoframe object
 switzerland <- geoframe(domain = mesh)
-
-
-## ----load data----------------------------------------------------------------
-## [DATA]
-# Load the data
 data <- read.table(file = "../data/QSRPDE_2D/QSRPDE_2D_data.txt", header = TRUE)
 head(data)
-
-# Add layer with data to the geoframe object
-switzerland$insert(layer = "rainfall", type = "point", geo = c("lon", "lat"), 
-                   data = data)
+switzerland$insert(
+  layer = "rainfall", type = "point", geo = c("lon", "lat"),  data = data
+)
 switzerland
 
-# Variable names
-names(switzerland[["rainfall"]])
+names(switzerland[["rainfall"]])              # Variable names
+ncol(switzerland[["rainfall"]])               # Number of variables
+head(gf_locations(switzerland[["rainfall"]])) # (lon, lat) of measurements
 
-# Number of variables
-ncol(switzerland[["rainfall"]])
+map1 <- 
+  mapview(
+    switzerland[["rainfall"]], varnames = "rainfall", 
+    crs = 4326, color_palettes = list("mako"), na.color = "transparent",
+    layer.name = "rainfall") +
+  mapview(
+    boundary_sf, 
+    col.regions = "transparent", alpha.regions = 0, col = "black", lwd = 1.5, 
+    layer.name = "domain", legend = FALSE
+  )
 
-# Locations of measurement stations (lon, lat)
-head(gf_locations(switzerland[["rainfall"]]))
-
-
-## ----mapview_data, fig.width=8.3, fig.height=5--------------------------------
-map1 <- mapview(switzerland[["rainfall"]], varnames = "rainfall", crs = 4326,
-                color_palettes = list("mako"), na.color = "transparent",
-                layer.name = "rainfall") +
-  mapview(boundary_sf, col.regions = "transparent", alpha.regions = 0,
-          col = "black", lwd = 1.5, layer.name = "domain", legend = FALSE)
-
-map2 <- mapview(switzerland[["rainfall"]], crs = 4326, varnames = "log.rainfall",
-                color_palettes = list("mako"), na.color = "transparent",
-                layer.name = "log.rainfall") +
-  mapview(boundary_sf, col.regions = "transparent", alpha.regions = 0,
-          col = "black", lwd = 1.5, layer.name = "domain", legend = FALSE)
+map2 <- 
+  mapview(
+    switzerland[["rainfall"]], varnames = "log.rainfall",
+    crs = 4326, color_palettes = list("mako"), na.color = "transparent",
+    layer.name = "log.rainfall") +
+  mapview(
+    boundary_sf, 
+    col.regions = "transparent", alpha.regions = 0, col = "black", lwd = 1.5, 
+    layer.name = "domain", legend = FALSE
+  )
 
 sync(map1, map2)
 
+######### PHYSICS 
+# Lf = -div(K grad(f))
 
-## -----------------------------------------------------------------------------
-## [DIFFUSION TENSOR]
 K <- matrix(
     c(1.229618413471819, 1.001009926596135, 1.001009926596135, 1.628164356689574),
     nrow = 2, ncol = 2, byrow = TRUE
 )
 
-
-## ----estimate_50, results="hide"----------------------------------------------
-## [PHYSICS-INFORMED QUANTILE REGRESSION]
-# Set up the finite element function (order 1)
+######### MODELING
+# physics informed quantile regression
 f <- fe_function(mesh, type = "P1")
-
-# Proposed value for the smoothing parameter
+# smoothing parameter proposals
 lambda_grid = 10^seq(from = -7, to = -3, by = 0.2)
 
-# Physics informed smoothing model
-m <- qsr(log.rainfall ~ f, data = switzerland, level = 0.5, 
-         penalty = fe_elliptic(K = K)
+m <- qsr(
+  log.rainfall ~ f, 
+  data = switzerland, 
+  level = 0.5, ## median
+  penalty = fe_elliptic(K = K)
 )
-
-# Physics informed smoothing fit with grid search for GCV minimization
 fit = m$fit(
-  calibrator = gcv(optimizer = grid_search(grid = lambda_grid),
-                   seed = 425)
+  calibrator = gcv(optimizer = grid_search(grid = lambda_grid), seed = 425)
 )
 
-
-## ----grid_GCV_50, fig.width=8.3, fig.height=5---------------------------------
-# GCV indices
+# inspect GCV curve
 gcv <- fit$values  
-
 # Optimal value selected for the smoothing parameter
 lambda_opt_grid <- fit$optimum
 lambda_opt_grid
-
 # Plot of the GCV curve
 par(family = "serif")
-plot(x = log10(lambda_grid), y = gcv, type = "b",
-     lwd = 2, xlab = TeX("$\\log_{10}(\\lambda)$"), ylab = "GCV")
+plot(
+  x = log10(lambda_grid), y = gcv, 
+  type = "b", lwd = 2, xlab = TeX("$\\log_{10}(\\lambda)$"), ylab = "GCV"
+)
 grid()
 abline(v = log10(lambda_opt_grid), lty = 2, lwd = 2, col = "royalblue")
-legend("bottomright", lty = 2, lwd = 2, col = "royalblue",
-       legend = TeX("$\\log_{10}(\\lambda_{grid})"))
+legend(
+  "bottomright", lty = 2, lwd = 2, col = "royalblue",
+  legend = TeX("$\\log_{10}(\\lambda_{grid})")
+)
 
+## median field plot
+map_log50 <- 
+  mapview(
+    f, 
+    crs = 4326, col.regions = mako, na.color = "transparent", 
+    layer.name = "log.rainfall") +
+  mapview(
+    boundary_sf, 
+    col.regions = "transparent", alpha.regions = 0, col = "black", lwd = 1.5, 
+    layer.name = "domain", legend = FALSE
+  )
 
-## ----mapview_50, fig.width=8.3, fig.height=5----------------------------------
-# Interactive plot
-map_log50 <- mapview(f, crs = 4326, col.regions = mako,
-                     na.color = "transparent", layer.name = "log.rainfall") +
-         mapview(boundary_sf, col.regions = "transparent", alpha.regions = 0,
-                 col = "black", lwd = 1.5, layer.name = "domain", legend = FALSE)
-
-map_50 <- mapview(exp(f), crs = 4326, col.regions = mako,
-                 na.color = "transparent", layer.name = "rainfall") +
-         mapview(boundary_sf, col.regions = "transparent", alpha.regions = 0,
-                col = "black", lwd = 1.5, layer.name = "domain", legend = FALSE)
+map_50 <- 
+  mapview(
+    exp(f), 
+    crs = 4326, col.regions = mako, na.color = "transparent", 
+    layer.name = "rainfall") +
+  mapview(
+    boundary_sf, 
+    col.regions = "transparent", alpha.regions = 0, col = "black", lwd = 1.5, 
+    layer.name = "domain", legend = FALSE
+  )
 
 sync(map_log50, map_50)
 
-
-## ----estimate_90, results="hide"----------------------------------------------
-## [PHYSICS-INFORMED QUANTILE REGRESSION]
-# Set up the finite element function (order 1)
+# 90% quantile estimation
 f <- fe_function(mesh, type = "P1")
-
-# Proposed value for the smoothing parameter
 lambda_grid = 10^seq(from = -7, to = -4, by = 0.2)
-
-# Physics informed smoothing model
-m <- qsr(log.rainfall ~ f, data = switzerland, level = 0.9, 
-         penalty = fe_elliptic(K = K)
+m <- qsr(
+  log.rainfall ~ f, 
+  data = switzerland, 
+  level = 0.9, ## 90% quantile
+  penalty = fe_elliptic(K = K)
 )
-
-# Physics informed smoothing fit with grid search for GCV minimization
 fit = m$fit(
-  calibrator = gcv(optimizer = grid_search(grid = lambda_grid),
-                   seed = 425)
+  calibrator = gcv(optimizer = grid_search(grid = lambda_grid), seed = 425)
 )
 
-
-## ----grid_GCV_90, fig.width=8.3, fig.height=5---------------------------------
-# GCV indices
+# inspect GCV curve
 gcv <- fit$values  
-
 # Optimal value selected for the smoothing parameter
 lambda_opt_grid <- fit$optimum
 lambda_opt_grid
-
 # Plot of the GCV curve
 par(family = "serif")
-plot(x = log10(lambda_grid), y = gcv, type = "b",
-     lwd = 2, xlab = TeX("$\\log_{10}(\\lambda)$"), ylab = "GCV")
+plot(
+  x = log10(lambda_grid), y = gcv, 
+  type = "b", lwd = 2, xlab = TeX("$\\log_{10}(\\lambda)$"), ylab = "GCV"
+)
 grid()
 abline(v = log10(lambda_opt_grid), lty = 2, lwd = 2, col = "royalblue")
-legend("bottomright", lty = 2, lwd = 2, col = "royalblue",
-       legend = TeX("$\\log_{10}(\\lambda_{grid})"))
+legend(
+  "bottomright", lty = 2, lwd = 2, col = "royalblue",
+  legend = TeX("$\\log_{10}(\\lambda_{grid})")
+)
 
+map_log90 <- 
+  mapview(
+    f, 
+    crs = 4326, col.regions = mako, na.color = "transparent", 
+    layer.name = "log.rainfall") +
+  mapview(
+    boundary_sf, 
+    col.regions = "transparent", alpha.regions = 0, col = "black", lwd = 1.5, 
+    layer.name = "domain", legend = FALSE
+  )
 
-## ----mapview_90, fig.width=8.3, fig.height=5----------------------------------
-# Interactive plot
-map_log90 <- mapview(f, crs = 4326, col.regions = mako,
-                     na.color = "transparent", layer.name = "log.rainfall") +
-         mapview(boundary_sf, col.regions = "transparent", alpha.regions = 0,
-                 col = "black", lwd = 1.5, layer.name = "domain", legend = FALSE)
-
-map_90 <- mapview(exp(f), crs = 4326, col.regions = mako,
-                 na.color = "transparent", layer.name = "rainfall") +
-         mapview(boundary_sf, col.regions = "transparent", alpha.regions = 0,
-                col = "black", lwd = 1.5, layer.name = "domain", legend = FALSE)
+map_90 <- 
+  mapview(
+    exp(f), 
+    crs = 4326, col.regions = mako, na.color = "transparent", 
+    layer.name = "rainfall") +
+  mapview(
+    boundary_sf, 
+    col.regions = "transparent", alpha.regions = 0, col = "black", lwd = 1.5, 
+    layer.name = "domain", legend = FALSE
+  )
 
 sync(map_log90, map_90)
-
